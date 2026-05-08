21b7596a268277e4ede2245665745e7220bfa56f
hiroaki003ono <275902077+hiroaki003ono@users.noreply.github.com>
2026-05-08 18:41:24 +0900

    feat: /hello と /hello/{name} ルートを追加

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function() {
+    return 'こんにちは、Laravel';
+});
+
+Route::get('/hello/{name}', function($name) {
+    return 'こんにちは、' . $name . 'さん!';
+});
