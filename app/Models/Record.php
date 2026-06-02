85e9e0c2a1d3c656178cc4cd8dc81c8cc033f5c9
hiroaki003ono <275902077+hiroaki003ono@users.noreply.github.com>
2026-06-02 17:10:23 +0900

    feat: 収支データをユーザーごとに分離

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -3,14 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Record extends Model
 {
     protected $fillable = [
+        'user_id',
         'date',
         'category',
         'description',
         'amount',
         'type',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
